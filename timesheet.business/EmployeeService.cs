729be16012c25921f35b4af64d1995c6aa07fc1e
azizmohammed49 <61844621+azizmohammed49@users.noreply.github.com>
2020-03-06 08:00:35 +0530

    Add files via upload

---
+++ timesheet.business/EmployeeService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using timesheet.data;
+using timesheet.model;
+
+namespace timesheet.business
+{
+    public class EmployeeService : IDisposable
+    {
+        public TimesheetDb db { get; }
+        public EmployeeService(TimesheetDb dbContext)
+        {
+            this.db = dbContext;
+        }
+
+        public IEnumerable<Employee> GetEmployees()
+        {
+            var emps = db.Employees.ToList();
+            return emps;
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+    }
+}
